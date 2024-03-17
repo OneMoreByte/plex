191145732e716574ba588cb60d908e391318e69f
Jack Hildebrandt <jack@jackhil.de>
2024-03-17 16:57:37 -0500

    tere

--- scripts/check_plex_tag.py
+++ scripts/check_plex_tag.py
@@ -63,7 +63,7 @@ def update_repo_version(new_version: tuple[semver.Version, str]):
         "email": "admin@jackhil.de",
         "content": base64.b64encode(
             (
-                f"SEM_VER={new_version[0].split('+')[0]}\nSEM_VER_BUILD={new_version[0].replace('+', '-')}\nDOCKER_TAG={new_version[1]}\n"
+                f"SEM_VER={str(new_version[0]).split('+')[0]}\nSEM_VER_BUILD={str(new_version[0]).replace('+', '-')}\nDOCKER_TAG={new_version[1]}\n"
             ).encode()
         ).decode(),
     }
